a50e498b222155f9448ab0a3bb3313e9781d8a50
Anjany Pandey <anjany.pandey06@gmail.com>
2024-11-28 14:33:33 +0530

    bug fix

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const exerciseOptions = {
   export const youtubeOptions = {
     method: 'GET',
     headers: {
-      'x-rapidapi-key': '55f76791c3msh2abf30b33d3862cp10d788jsn45c36aa54bdd1',
+      'x-rapidapi-key': '55f76791c3msh2abf30b33d3862cp10d788jsn45c36aa54bdd',
       'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
